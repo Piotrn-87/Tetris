62ac9ceba248ff2eaafab03f907383586b83e2b9
Piotr Napieralski <piotrn-87@o2.pl>
2020-01-26 18:53:49 +0100

    Feat: Create GameBoard

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,90 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // registration failed
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
-// place your code below
+console.log(`Hello Tetromino!`);
 
+let canvas;
+let ctx;
+let gameBoardArrayHeight = 20;
+let gameBoardArrayWidth = 12;
+let startX = 4;
+let startY = 0;
+let coordinateArray = [...Array(gameBoardArrayHeight)].map(e =>
+  Array(gameBoardArrayWidth).fill(0)
+);
+let currentTetramino = [
+  [1, 0],
+  [0, 1],
+  [1, 1],
+  [2, 1]
+];
 
-console.log(`Hello world!`);
+let tetrominos = [];
+let tetrominosColors = ["violet", "cyan", "blue", "green", "red"];
+let currentTetrominoColor;
 
+let gameBoardArray = [...Array(gameBoardArrayHeight)].map(e =>
+  Array(gameBoardArrayWidth).fill(0)
+);
 
+class Coordinates {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+document.addEventListener("DOMContentLoaded", SetupCanvas);
+
+function CreateCoordArray() {
+  let i = 0,
+    j = 0;
+  for (let y = 9; y <= 446; y += 23) {
+    for (let x = 11; x <= 264; x += 23) {
+      coordinateArray[i][j] = new Coordinates(x, y);
+      i++;
+    }
+    j++;
+    i = 0;
+  }
+}
+function SetupCanvas() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = 936;
+  canvas.height = 956;
+
+  ctx.scale(2, 2);
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(8, 8, 280, 462);
+
+  CreateCoordArray();
+  DrawTetromino();
+}
+
+function DrawTetromino() {
+  for (let i = 0; i < currentTetramino.length; i++) {
+    let x = currentTetramino[i][0] + startX;
+    let y = currentTetramino[i][1] + startY;
+  }
+}
