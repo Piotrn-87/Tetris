b2b8bbe4a9ea9b48f4962c320575628351fb83f4
Piotr Napieralski <piotrn-87@o2.pl>
2020-02-02 18:16:24 +0100

    Feat: Createa HittingTheWall function

--- src/js/main.js
+++ src/js/main.js
@@ -26,10 +26,11 @@ let gameBoardArrayHeight = 20;
 let gameBoardArrayWidth = 12;
 let startX = 4;
 let startY = 0;
+
 let coordinateArray = [...Array(gameBoardArrayHeight)].map(e =>
   Array(gameBoardArrayWidth).fill(0)
 );
-let currentTetramino = [
+let currentTetromino = [
   [1, 0],
   [0, 1],
   [1, 1],
@@ -37,24 +38,21 @@ let currentTetramino = [
 ];
 
 let tetrominos = [];
-let tetrominosColors = [
+let shapesColors = [
   "violet",
   "cyan",
   "blue",
   "green",
   "red",
   "orange",
-  "yellow"
+  "coral"
 ];
-let currentTetrominoColor = "green";
-
-let gameBoardArray = [...Array(gameBoardArrayHeight)].map(e =>
-  Array(gameBoardArrayWidth).fill(0)
-);
+let currentTetrominoColor;
+let gameBoardArray = [...Array(20)].map(e => Array(12).fill(0));
 
 let DIRECTION = {
   IDEL: 0,
-  DOWN: 0,
+  DOWN: 1,
   LEFT: 2,
   RIGHT: 3
 };
@@ -72,8 +70,8 @@ document.addEventListener("DOMContentLoaded", SetupCanvas);
 function CreateCoordArray() {
   let i = 0,
     j = 0;
-  for (let y = 9; y <= 446; y += 23) {
-    for (let x = 11; x <= 264; x += 23) {
+  for (let y = 10; y <= 546; y += 12) {
+    for (let x = 20; x <= 240; x += 12) {
       coordinateArray[i][j] = new Coordinates(x, y);
       i++;
     }
@@ -84,103 +82,117 @@ function CreateCoordArray() {
 function SetupCanvas() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  canvas.width = 936;
-  canvas.height = 956;
+  canvas.width = 530;
+  canvas.height = 556;
 
-  ctx.scale(2, 2);
+  // ctx.scale(2, 2);
 
   ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(6, 6, canvas.width, canvas.height);
 
   ctx.strokeStyle = "black";
-  ctx.strokeRect(8, 8, 280, 462);
+  ctx.strokeRect(4, 8, 520, 552);
 
   document.addEventListener("keydown", handleKeyPress);
 
-  CreateTetramino();
-
+  CreateShapes();
+  CreateTetromino();
   CreateCoordArray();
   DrawTetromino();
 }
 
 function DrawTetromino() {
-  for (let i = 0; i < currentTetramino.length; i++) {
-    let x = currentTetramino[i][0] + startX;
-    let y = currentTetramino[i][1] + startY;
+  for (let i = 0; i < currentTetromino.length; i++) {
+    let x = currentTetromino[i][0] + startX;
+    let y = currentTetromino[i][1] + startY;
     gameBoardArray[x][y] = 1;
-    let coorX = (coorX = coordinateArray[x][y].x);
-    let coorY = (coorY = coordinateArray[x][y].y);
-    ctx.fillStyle = currentTetraminoColor;
-    ctx.fillRect(coorX, coorY, 21, 21);
+    let coorX = coordinateArray[x][y].x;
+    let coorY = coordinateArray[x][y].y;
+    ctx.fillStyle = currentTetrominoColor;
+    ctx.fillRect(coorX, coorY, 10, 10);
+    console.log("COLOR", currentTetrominoColor);
   }
 }
 
 function handleKeyPress(key) {
   if (key.keyCode === 65) {
     direction = DIRECTION.LEFT;
-    DeleteTetramino();
-    startX--;
-    DrawTetromino();
+    if (!HittingTheWall()) {
+      DeleteTetromino();
+      startX--;
+      DrawTetromino();
+    }
   } else if (key.keyCode === 68) {
     direction = DIRECTION.RIGHT;
-    startX++;
-    DrawTetromino();
+    if (!HittingTheWall()) {
+      DeleteTetromino();
+      startX++;
+      DrawTetromino();
+    }
   } else if (key.keyCode === 83) {
     direction = DIRECTION.DOWN;
+    DeleteTetromino();
     startY++;
     DrawTetromino();
   }
 }
-function DeleteTetramino() {
-  for (let i = 0; i < currentTetramino.length; i++) {
-    let x = currentTetramino[i][0] + startX;
-    let y = currentTetramino[(1)[1]] + startY;
-    gameBoardArrayHeight[x][y] = 0;
+function DeleteTetromino() {
+  for (let i = 0; i < currentTetromino.length; i++) {
+    let x = currentTetromino[i][0] + startX;
+    let y = currentTetromino[i][1] + startY;
+    gameBoardArray[x][y] = 0;
     let coorX = coordinateArray[x][y].x;
     let coorY = coordinateArray[x][y].y;
     ctx.fillStyle = "white";
-    ctx.fillRect(coorX, coorY, 21, 21);
+    ctx.fillRect(coorX, coorY, 20, 20);
   }
 }
 
-function CreateTetramino() {
+function CreateShapes() {
+  // T Shape
   tetrominos.push([
     [1, 0],
     [0, 1],
     [1, 1],
     [2, 1]
   ]);
-  tetraminos.push([
+  // I Shape
+  tetrominos.push([
     [0, 0],
     [1, 0],
     [2, 0],
     [3, 0]
   ]);
-  tetraminos.push([
+  // J Shape
+  tetrominos.push([
     [0, 0],
     [0, 1],
     [1, 1],
     [2, 1]
   ]);
-  tetraminos.push([
+  // Square Shape
+  tetrominos.push([
     [0, 0],
     [1, 0],
     [0, 1],
     [1, 1]
   ]);
-  tetraminos.push([
+  // L Shape
+  tetrominos.push([
     [2, 0],
     [0, 1],
     [1, 1],
     [2, 1]
   ]);
-  tetraminos.push([
+  // S Shape
+  tetrominos.push([
     [1, 0],
     [2, 0],
     [0, 1],
     [1, 1]
   ]);
-  tetraminos.push([
+  // Z Shape
+  tetrominos.push([
     [0, 0],
     [1, 0],
     [1, 1],
@@ -188,8 +200,22 @@ function CreateTetramino() {
   ]);
 }
 
-function CreateTetramino() {
-  let randomTetramino = Math.floor(Math.random() * tetraminos.length);
-  currentTetramino = tetraminos[randomTetramino];
-  currentTetrominoColor = currentTetraminoColor[randomTetramino];
+function CreateTetromino() {
+  let randomTetromino = Math.floor(Math.random() * tetrominos.length);
+  currentTetromino = tetrominos[randomTetromino];
+  currentTetrominoColor = shapesColors[randomTetromino];
+  console.log("Random", randomTetromino);
+  console.log("Length", tetrominos.length);
+}
+
+function HittingTheWall() {
+  for (let i = 0; i < currentTetromino.length; i++) {
+    let newX = currentTetromino[i][0] + startX;
+    if (newX <= 0 && direction === DIRECTION.LEFT) {
+      return true;
+    } else if (newX >= 20 && direction === DIRECTION.RIGHT) {
+      return true;
+    }
+  }
+  return false;
 }
